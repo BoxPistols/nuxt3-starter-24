9f850161225a70b2333f0696265cf0a7e7da5713
a.ito <10333049+BoxPistols@users.noreply.github.com>
2024-03-14 11:16:02 +0900

    line test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,52 @@
 // nuxt.config.ts
-
 export default defineNuxtConfig({
   app: {
     head: {
+      htmlAttrs: {
+        lang: 'ja',
+        prefix: 'og: http://ogp.me/ns#'
+      },
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       title: 'My Stylish Website',
       meta: [
-        // LINE https://placehold.jp/20d523/ffffff/150x150.png?text=LINE
         { name: 'description', content: 'A stylish website built with Nuxt' },
         { name: 'keywords', content: 'Nuxt, Vue, website, stylish, modern' },
         { name: 'author', content: 'Your Name' },
         { name: 'theme-color', content: '#ff5a5f' },
-        { property: 'og:title', content: 'My Stylish Website' },
-        {
-          property: 'og:description',
-          content: 'A stylish website built with Nuxt'
-        },
         { property: 'og:type', content: 'website' },
         { property: 'og:url', content: 'https://yourwebsite.com' },
         {
-          property: 'og:image',
-          content:
-            'https://images.pexels.com/photos/2891250/pexels-photo-2891250.jpeg?auto=compress&cs=tinysrgb&w=600'
+          property: 'og:title',
+          //@ts-ignore
+          content: () =>
+            process.server
+              ? useRequestHeaders()['user-agent']?.includes('Line')
+                ? 'LINE用のタイトル'
+                : 'My Stylish Website'
+              : 'My Stylish Website'
         },
+        {
+          property: 'og:description',
+          //@ts-ignore
+          content: () =>
+            process.server
+              ? useRequestHeaders()['user-agent']?.includes('Line')
+                ? 'LINE用の説明文'
+                : 'A stylish website built with Nuxt'
+              : 'A stylish website built with Nuxt'
+        },
+        {
+          property: 'og:image',
+          //@ts-ignore
+          content: () =>
+            process.server
+              ? useRequestHeaders()['user-agent']?.includes('Line')
+                ? 'https://placehold.jp/20d523/ffffff/150x150.png?text=LINE'
+                : 'https://placehold.jp/ff5a5f/ffffff/150x150.png?text=OGP'
+              : 'https://placehold.jp/ff5a5f/ffffff/150x150.png?text=OGP'
+        },
+        // Twitter Card
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:title', content: 'My Stylish Website' },
         {
